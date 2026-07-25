52fb8f9be9419aefa64987ddd41ae7f32ee4f2d1
AndersonnTech <anderson2csilva98@gmail.com>
2026-07-25 15:55:22 -0300

    feat(infrastructure) The repository logic was created using 'RepositoryRestResource' to provide the endpoints, and an 'EventHandler' was also used.

---
+++ src/main/java/project/compliance/infrastructure/persistence/event/CompanyEventHandler.java
@@ -0,0 +1,19 @@
+package project.compliance.infrastructure.persistence.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+import project.compliance.infrastructure.persistence.entity.CompanyEntity;
+
+@Component
+@RepositoryEventHandler
+public class CompanyEventHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(CompanyEventHandler.class);
+
+    @HandleAfterCreate
+    public void handlerAfterCreateEvent(CompanyEntity entity) {
+        LOG.info("handleAfterCreateEvent {}", entity);
+    }
+}
